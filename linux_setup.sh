9fa5ded0087f216c7a58da77969d88ca9aa83c06
kitswas <90329875+kitswas@users.noreply.github.com>
2022-12-06 17:31:42 +0530

    Update linux_setup.sh

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,1 +1,1 @@
-sudo apt upgrade ffmpeg libglew-dev libsdl2-dev libsdl2-image-dev libpng-dev libavformat-dev libswscale-dev libavcodec-dev g++-8 -y
+sudo apt upgrade libavdevice-dev ffmpeg libglew-dev libsdl2-dev libsdl2-image-dev libpng-dev libavformat-dev libswscale-dev libavcodec-dev g++-8 -y
